da26b1a6a5025fde20a561e05771d6897fe44a0b
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2022-02-21 10:32:16 +0545

    User registration added with token generation

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express =require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const errorMiddleware = require('./middleware/error')
+const errorMiddleware = require("./middleware/error");
 
-app.use(express.json())
+app.use(express.json());
 
 // Route imports
-const book = require('./routes/bookRoute')
+const book = require("./routes/bookRoute");
+const user = require("./routes/userRoute");
 
-app.use('/api/v1', book)
+app.use("/api/v1", book);
+app.use("/api/v1", user);
 
 // Middleware for Errors
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-
-module.exports = app
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,26 @@
+const Book = require("../models/bookModel");
+const ErrorHandler = require("../utils/errorhandler");
+const User = require("../models/userModel");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+// Register a user
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "This is sample id",
+      url: "profilepicUrl",
+    },
+  });
+
+  const token = user.getJWTToken()
+
+  res.status(201).json({
+    success: true,
+    token,
+  });
+});
